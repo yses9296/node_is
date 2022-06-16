bb16df9521f0fd25808c93c7bf8f577df0b1c7e8
Eunseo Choi <yses9296@gmail.com>
2022-06-16 23:14:05 +0900

    node js 03

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "oswald" : ['Oswald', 'sans-serif']
+      }
+    }
   },
   plugins: [],
 }
